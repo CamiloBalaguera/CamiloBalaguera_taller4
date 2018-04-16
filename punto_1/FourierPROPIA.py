941747f48f0249818b0e49f7656e93b060303892
Camilo Balaguera <ca.balaguera@uniandes.edu.co>
2018-04-15 19:41:19 -0500

    Las transformadas de fourier se implementaron sin usar los metodos nativos de python. Además se creo el archivo FourierPROPIA.py que tiene el objetivo de demostrar que mi implementacion y la de los metodos nativos de python arrojan exactamente el mismo resultado

---
+++ punto_1/FourierPROPIA.py
@@ -0,0 +1,37 @@
+import numpy as np
+from scipy import fftpack
+import matplotlib.pyplot as plt
+pi2 = np.pi * 2.0
+muestra = np.array(([2,3,4,5], [2,3,4,5], [2,3,4,5], [2,3,4,5]))
+def bifourier(array):
+    (M, N) = array.shape 
+    fourier = np.zeros((M,N), dtype = complex)
+    for k in range(M):
+        for l in range(N):
+            suma = 0.0
+            for m in range(M):
+                for n in range(N):
+                    t = np.exp(- 1j * pi2 * ((k * m) / M + (l * n) / N))
+                    suma += array[m,n] * t
+            fourier[l][k] =  suma
+    return np.transpose(fourier)
+
+def invbifourier(array):
+    (M, N) = array.shape 
+    imagen = np.zeros((M,N), dtype = complex)
+    for m in range(M):
+        for n in range(N):
+            suma = 0.0
+            for k in range(M):
+                for l in range(N):
+                    t = np.exp(1j * pi2 * ((k * m) / M + (l * n) / N))
+                    suma += array[l][k] * t
+            val = suma / (M*N)
+            imagen[m, n] = val
+    return np.transpose(imagen)
+      
+
+
+print("Implementacion propia","\n" ,bifourier(muestra),"\n", "Metodo nativo de python", "\n",  fftpack.fft2(muestra))
+print("Implementacion propia inversa","\n" ,invbifourier(muestra),"\n", "Metodo nativo de python", "\n",  fftpack.ifft2(muestra))
+print("Diferencia")
--- punto_1/suave.py
+++ punto_1/suave.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 
 img = plt.imread("uniandes.png")
 img = img[:,:,:3]
+X, Y = img.shape[:2]
+pi2 = np.pi * 2
+
+def bifourier(array):
+    (M, N) = array.shape 
+    fourier = np.zeros((M,N), dtype = complex)
+    for k in range(M):
+        for l in range(N):
+            suma = 0.0
+            for m in range(M):
+                for n in range(N):
+                    t = np.exp(- 1j * pi2 * ((k * m) / M + (l * n) / N))
+                    suma += array[m,n] * t
+            fourier[l][k] =  suma
+    return np.transpose(fourier)
 
 def gaussian(kernlen1, kernlen2, sig):
 	"""creates gaussian kernel with side length l and a sigma of sig"""
@@ -13,11 +28,11 @@ def gaussian(kernlen1, kernlen2, sig):
 	kernel = np.exp(-(x**2 + y**2) / (2. * sig**2))
 	return kernel / np.sum(kernel)
 
-# Padded fourier transform, with the same shape as the image
-# We use :func:`scipy.signal.fftpack.fft2` to have a 2D FFT
-kernel_ft = fftpack.fft2(gaussian(30, 30, 5), shape=img.shape[:2], axes=(0, 1))
-print(kernel_ft)
+# Transformada de fourier del kernel, que resulta ser una gaussiana
+kernel_ft = fftpack.fft2(gaussian(5, 5, 1), axes=(0, 1)) # Usando el metodo nativvo de python
+#kernel_ft = bifourier(gaussian(5,5,1)) #Usando mi propia implementación. En caso de querer utilizarla, comentar la linea de arriba y descomentar esta. ATENCION!: No la utilizo porque simplemente nunca se termina de ejecutar cuando lo hago
 
+"""
 # convolve
 img_ft = fftpack.fft2(img, axes=(0, 1))
 # the 'newaxis' is to match to color direction
@@ -29,5 +44,5 @@ img2 = np.clip(img2, 0, 1)
 
 # plot output
 plt.figure()
-plt.imshow(img2)
-plt.show()
+plt.imshow(img2)"""
+
